c54f48b9dae308142be38fb15657ad2f13fc1cb3
ossipova <ossipova11@gmail.com>
2023-01-23 16:31:21 -0600

    HW 4

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products.views import main_view, products_view, product_detail_view
+from OnlineStore.settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_view),
     path('products/', products_view),
     path('products/<int:product_id>/', product_detail_view)
+
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
